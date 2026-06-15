82230f9a9a1e7a886af533bf3c067ac606905b91
klawi1121 <qcaoile@gmail.com>
2026-06-16 01:46:34 +0800

    Draft: added method logic inside strings function

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -13,6 +13,13 @@ function capitalize(str) {
   // Hint: Get the first character with str[0] or str.charAt(0)
   // Convert it to uppercase, then concatenate with the rest of the string
   // Don't forget to handle empty strings!
+  if(str.length === 0) {
+    return '';
+  }
+  const capitalized = str[0].toUpperCase() + str.slice(1);
+  return capitalized;
+
+  //or should i change it into str.charAt(0).toUpperCase() so it safely return an empty string?
 }
 
 /**
@@ -24,6 +31,10 @@ function reverse(str) {
   // TODO: Implement reverse
   // Hint: Convert string to array with split(''), reverse it, join back
   // Or you can use a loop to build the reversed string
+  let word = str.split('');
+  let reversedword = word.reverse();
+  let combinedletters = reversedword.join('');
+  return combinedletters;
 }
 
 /**
@@ -36,6 +47,8 @@ function isPalindrome(str) {
   // Hint: Convert to lowercase first for case-insensitive comparison
   // Compare the string with its reversed version
   // You can use the reverse function you just wrote!
+  let convertword = str.toLowerCase();
+  return convertword === reverse(convertword);
 }
 
 /**
@@ -48,6 +61,14 @@ function wordCount(str) {
   // Hint: Trim whitespace first, then split by spaces
   // Don't forget to handle empty strings!
   // Be careful with multiple spaces between words
+  if(str.trim() === '') {
+    return 0;
+  }   
+  let word = str.trim().split(/\s+/);
+  return word.length; 
+
+  // or i put split on return statement?
+  
 }
 
 // Export all functions
